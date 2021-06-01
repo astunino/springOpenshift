76bb97b80c9b3cbfa5d9bd31770f79e5e64bb75a
Astunino <astunino@gmail.com>
2021-06-01 14:42:18 -0300

    Nuevo commit

---
+++ src/main/java/SpringRestAngular/SpringRestAngularApplication.java
@@ -0,0 +1,13 @@
+package SpringRestAngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SpringRestAngular/SpringRestAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringRestAngular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
